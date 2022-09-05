fd72cc58018931efa62d8711e2ec4152f34c85e4
Phan Tân <tanphan@Phans-MacBook-Pro.local>
2022-09-06 02:06:06 +0700

    daily studt dfs invert tree

---
+++ problems/tree/invert_binary_tree.rb
@@ -0,0 +1,29 @@
+<<-Doc
+Given the root of a binary tree, invert the tree, and return its root.
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Doc
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  return if root == nil
+
+  left = root.left
+  right = root.right
+
+  root.left = invert_tree(right)
+  root.right = invert_tree(left)
+
+  root
+end
